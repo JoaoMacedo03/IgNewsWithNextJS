637f9f03931b2f8d2bdf85907b27f42683b2db6e
Joao Macedo <joaovictormacedo91@gmail.com>
2021-12-23 15:45:15 -0300

    feat: First Server Side Rendering API call
    It was added at the Home page, an API call for the stripe services to
    the get the price of the subscription and show it to the final user.
    This API call, did not happened at the client side(Browser), but at
    nodeJS server instanciated by NextJS

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,17 @@
+import { GetServerSideProps } from 'next';
+import { stripe } from '../services/stripe'
 import Head from 'next/head';
 import { SubscribeButton } from '../components/SubscribeButton';
 import styles from './home.module.scss';
 
-export default function Home() {
+interface HomeProduct {
+  product: {
+    priceId: string;
+    amount: number;
+  }
+}
+
+export default function Home({ product }: HomeProduct) {
   return (
     <>
       <Head>
@@ -14,13 +23,31 @@ export default function Home() {
           <h1>News about the <span>React</span> World.</h1>
           <p>
             Get access to all the publications <br />
-            <span>for $9.90 month</span>
+            <span>for {product.amount} month</span>
           </p>
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId} />
         </section>
 
         <img src="/images/avatar.svg" alt="Girl Coding" />
       </main>    
     </>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const price = await stripe.prices.retrieve('price_1K9vrvIJZRU4Xe9GZU5jWEbh')
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD'
+    }).format((price.unit_amount / 100)),
+  }
+
+  return {
+    props: {
+      product
+    }
+  }
 }
