64ff3ba99235c811db813038e25035f7613129ae
Joao Macedo <joaovictormacedo91@gmail.com>
2021-12-23 16:21:27 -0300

    feat: Changed SSR to SSG
    It was changed in the Home Page from Server-side Rendering to Static
    Site Generation, cause the information on this can be static. It's not
    going to change content of the page for every single user that access
    it.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,12 @@
-import { GetServerSideProps } from 'next';
+import { GetStaticProps } from 'next';
 import { stripe } from '../services/stripe'
 import Head from 'next/head';
 import { SubscribeButton } from '../components/SubscribeButton';
 import styles from './home.module.scss';
+
+//Client-side
+//Server-side
+//Static Site Generation
 
 interface HomeProduct {
   product: {
@@ -34,7 +38,7 @@ export default function Home({ product }: HomeProduct) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1K9vrvIJZRU4Xe9GZU5jWEbh')
 
   const product = {
@@ -48,6 +52,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       product
-    }
+    },
+    revalidate: 60 * 60 * 24, //24 hours
   }
 }
