7d984d4a7535c6d72c38960d61d62484a10e3982
Joao Macedo <joaovictormacedo91@gmail.com>
2021-12-27 20:01:20 -0300

    feat: Starting the development of stripe checkout session, so the user can subscribe to our app.

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,14 +1,33 @@
+import { signIn, signOut, useSession } from 'next-auth/react'
 import styles from './styles.module.scss'
 
 interface SubscribeButtonProps {
   priceId: string;
 }
 
+//getServerSideProps (SSR - Server Side Rendering)
+//getStaticProps (SSG - Static Site Generation)
+//API routes
+
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  const { data: session } = useSession()
+
+  function handleSubcribe() {
+    if(!session) {
+      signIn('github')
+      return
+    }
+
+    //Criação da checkout session
+
+
+  }
+
   return (
     <button
       type="button"
       className={styles.subscribeButton}
+      onClick={handleSubcribe}
     >
       Subscribe Now
     </button>
